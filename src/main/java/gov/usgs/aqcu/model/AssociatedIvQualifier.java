b2b1822f6a187c7a23226855a351e1ebe40e8db3
Zack Moore <zmoore@usgs.gov>
2018-11-02 12:22:34 -0500

    Added qualifier start/end dates. Added associated test.

--- src/main/java/gov/usgs/aqcu/model/AssociatedIvQualifier.java
+++ src/main/java/gov/usgs/aqcu/model/AssociatedIvQualifier.java
@@ -1,12 +1,18 @@
 package gov.usgs.aqcu.model;
 
+import java.time.Instant;
+
 import com.aquaticinformatics.aquarius.sdk.timeseries.servicemodels.Publish.Qualifier;
 
 public class AssociatedIvQualifier {
 	private String identifier;
+	private Instant startDate;
+	private Instant endDate;
 
 	public AssociatedIvQualifier(Qualifier qual) {
 		this.identifier = qual.getIdentifier();
+		this.startDate = qual.getStartTime();
+		this.endDate = qual.getEndTime();
 	}
 
 	public String getIdentifier() {
@@ -15,5 +21,21 @@ public class AssociatedIvQualifier {
 
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
+	}
+
+	public Instant getStartDate() {
+		return startDate;
+	}
+
+	public Instant getEndDate() {
+		return endDate;
+	}
+
+	public void setStartDate(Instant startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(Instant endDate) {
+		this.endDate = endDate;
 	}
 }
